b8aefe8d19c84b427812b8fbeef799d307f85437
Farhan Khan <mdfarhanahmadkhan7786@gmail.com>
2026-02-20 22:22:48 +0530

    added the validation object with the help of pydantic

--- books_2.py
+++ books_2.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI, Body
-from pydantic import BaseModel
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 class Book:
     id: int
-    title: str
+    title: str 
     author: str
     description: str
     rating: int
@@ -17,33 +19,38 @@ class Book:
         self.description = description
         self.rating = rating
 
-class BookRequest(BaseModel):
-    id: int
-    title: str
-    author: str
-    description: str
-    rating: int
+class Bookrequest(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+    
 
+BOOKS = [Book(1, "Computer Science Pro", "codingwithfarhan", "A very nice Book", 5),
+         Book(2, "Be Fast With FastAPI", "codingwithfarhan", "This is a great book",5),
+         Book(3, "Master endpoints","codingwithfarhan", "A awesome book", 5),
+         Book(4, "HP1", "Author 1", "Book Description", 2),
+         Book(5, "HP2", "Author 2", "Book Description", 3),
+         Book(6, "HP3", "Author 3", "Book Description", 1)]
 
-
-
-
-BOOKS = [
-    Book(1, "Computer Science Pro", "Coding with Farhan", "Book Description", 5),
-    Book(2, "Be Fast with FastAPI", "Coding with Farhan", "A great book!", 5),
-    Book(3, "Master EndPoints", "Coding with Farhan", "An awsome book!", 5),
-    Book(4, "HP1", "Author 1", "Book Description", 2),
-    Book(5, "HP2", "Author 2", "Book Description", 3),
-    Book(6, "HP3", "Author 3", "Book Description", 1),
-]
 @app.get("/books")
-async def read_all_books():
+async def get_books():
     return BOOKS
 
-
-
-@app.post("/create_book")
-async def create_book(book_request: BookRequest):
+@app.post("/books")
+async def create_books(book_request: Bookrequest):
+    # print(type(book_request))
     new_book = Book(**book_request.model_dump())
     print(type(new_book))
-    BOOKS.append(new_book)
+    BOOKS.append(find_book_id(new_book))
+
+def find_book_id(book: Book):
+
+    # book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    if len(BOOKS) > 0:
+        book.id = BOOKS[-1].id + 1
+    else:
+        book.id = 1
+
+    return book

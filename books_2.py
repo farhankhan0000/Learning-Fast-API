0b395074350d4c4ec56376d19738dd187bd0bacc
Farhan Khan <mdfarhanahmadkhan7786@gmail.com>
2026-02-23 18:55:45 +0530

    added published date option in book

--- books_2.py
+++ books_2.py
@@ -11,13 +11,15 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description, rating, published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 class Bookrequest(BaseModel):
     id: Optional[int] = Field(description="ID is not needed in create", default=None)
@@ -25,6 +27,7 @@ class Bookrequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+    published_date: int = Field(gt=1999, lt=2031)
 
     model_config = {
         "json_schema_extra": {
@@ -32,22 +35,51 @@ class Bookrequest(BaseModel):
                 "title" : "A new Book",
                 "author": "codingwithfarhan",
                 "description": "A new description of a book",
-                "rating": 5
+                "rating": 5,
+                "published_date": 2029
             }
         }
     }
     
 
-BOOKS = [Book(1, "Computer Science Pro", "codingwithfarhan", "A very nice Book", 5),
-         Book(2, "Be Fast With FastAPI", "codingwithfarhan", "This is a great book",5),
-         Book(3, "Master endpoints","codingwithfarhan", "A awesome book", 5),
-         Book(4, "HP1", "Author 1", "Book Description", 2),
-         Book(5, "HP2", "Author 2", "Book Description", 3),
-         Book(6, "HP3", "Author 3", "Book Description", 1)]
+BOOKS = [Book(1, "Computer Science Pro", "codingwithfarhan", "A very nice Book", 5, 2030),
+         Book(2, "Be Fast With FastAPI", "codingwithfarhan", "This is a great book",5, 2030),
+         Book(3, "Master endpoints","codingwithfarhan", "A awesome book", 5, 2029),
+         Book(4, "HP1", "Author 1", "Book Description", 2, 2028),
+         Book(5, "HP2", "Author 2", "Book Description", 3, 2027),
+         Book(6, "HP3", "Author 3", "Book Description", 1, 2026)]
 
 @app.get("/books")
 async def get_books():
     return BOOKS
+
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+    
+
+        
+@app.get("/books/")
+async def read_book_by_rating(book_rating: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+
+    return books_to_return
+
+@app.get("/books/publish/")
+async def read_books_by_publish_date(published_date: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_to_return.append(book)
+
+    return books_to_return
+
+
 
 @app.post("/books")
 async def create_books(book_request: Bookrequest):
@@ -65,3 +97,16 @@ def find_book_id(book: Book):
         book.id = 1
 
     return book
+
+@app.put("/books/update_book")
+async def update_book(book: Bookrequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break

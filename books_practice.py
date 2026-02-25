cafd7d085741b34d1e75a741044e6d146bdb491d
Farhan Khan <mdfarhanahmadkhan7786@gmail.com>
2026-02-25 22:50:49 +0530

    created a book directoy on my own

--- books_practice.py
+++ books_practice.py
@@ -1,63 +1,92 @@
+from fastapi import FastAPI, Body
 from typing import Optional
-
-from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 class Book:
-    id : int
-    name : int
-    author: int
+    id: int
+    title: str
+    author: str
     rating: int
-    review: int
+    published_year: int
+    review: str
 
-    def __init__(self, id, name, author, rating, review):
+    def __init__(self, id, title, author, rating, published_year, review):
         self.id = id
-        self.name = name
+        self.title = title
         self.author = author
-        self.rating = rating
+        self.rating = rating 
+        self.published_year = published_year
         self.review = review
 
-class Bookrequest(BaseModel):
-    id: Optional[int] = Field(description="ID is not needed on create", default=None)
-    name: str = Field(min_length=3)
-    author: str = Field(min_length=2)
-    rating: int = Field(gt=1, lt=6)
-    review: str = Field(min_length=1, max_length=100)
+class book_request(BaseModel):
+    id: Optional[int] = Field(description= "Id is not needed when creating", default=None)
+    title: str = Field(min_lenght= 2, max_length= 200)
+    author: str = Field(min_length= 2, max_length= 200)
+    rating: int = Field(gt=0, lt=6)
+    published_year: int = Field(gt= 1800, lt= 2026)
+    review: str = Field(min_length= 5, max_length= 500)
 
-
-    model_config  = {
-        "json_schema_extra": {
-            "example": {
-                
-            }
+    model_config = {
+        "json_schema_extra" : {
+            "title" : "A New Book",
+            "author" : "author name",
+            "rating" : 5,
+            "published_year" : 1801,
+            "review" : "It was such a wonderful book"
         }
     }
 
-    
 
-BOOKS = [Book(1, "Metamorphosis", "Franz Kafka", 9, "Book that showed fragility of Human Identity"),
-         Book(2, "The Trial", "Franz Kafka", 8, "It was an awesome book"),
-         Book(3, "12 Rules For Life", "Jordan B Peterson", 10, "This Book is an antitode to chaos"),
-         Book(4, "Think and grow rich", "Napolean Hill", 8, "Clear the mindset and thinking about money")]
+Book_store = [Book(1, "Metamorphosis", "Franz Kafka", 4, 1915, "Fragility of Human Identity"),
+              Book(2, "12 Rules For Life", "Jordan B Peterson", 4, 2018, "Mix of Philosophy Human Phsycology"),
+              Book(3, "The Trial", "Franz Kafka", 3, 1925, "Didnt click much"),
+              Book(4, "Think and Grow Rich", "Napolean Hill", 4, 1937, "Good Book with Good principles"),
+              Book(5, "Crushing it", "Gary Vaynerchuk", 4, 2018, "Best if you want to create content")
+              ]
 
 
-@app.get("/books")
+@app.get("/Books/")
 async def get_books():
-    return BOOKS
+    return Book_store
 
-@app.post("/books")
-async def create_books(book_request: Bookrequest):
-    new_book = Book(**book_request.model_dump())
+@app.get("/Books")
+async def get_books(book_id: int):
+    for book in Book_store:
+        if book.id == book_id:
+            return book
+        
+@app.get("/Books/{book_rating}")
+async def get_book_by_rating(book_rating: int):
+    books_to_return = []
+    for book in Book_store:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
+@app.post("/create_book")
+async def create_books(book: book_request):
+    new_book = Book(**book.model_dump())
     print(type(new_book))
-    BOOKS.append(new_book)
+    Book_store.append(find_book_id(new_book))
 
-def find_id(book: Book):
 
-    if len(BOOKS) > 0:
-        book.id = BOOKS[-1].id + 1
+def find_book_id(book: Book):
+    if len(Book_store) > 0:
+        book.id = Book_store[-1] + 1
+
     else:
         book.id = 1
 
     return book
+
+@app.delete("/Books/{book_id}")
+async def delete_book_by_id(book_id: int ):
+    for i in range(len(Book_store)):
+        if Book_store[i].id == book_id:
+            Book_store.pop(i)
+            return
+
+
+
